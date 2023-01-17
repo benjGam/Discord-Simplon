4752a975d8fb4b37782cd41b4a1eccbb905b213e
Mecopi <mecopilive@gmail.com>
2023-01-17 10:33:19 +0100

    fix(command handling): fix command handling, parse modules

--- src/onboarding/index.ts
+++ src/onboarding/index.ts
@@ -1,8 +1,6 @@
-import { Client, Events, GatewayIntentBits} from "discord.js";
+import { Client, Events, GatewayIntentBits, Interaction } from "discord.js";
 import * as dotenv from "dotenv";
 import commands_handler from "./handlers/commands_handler.js";
-import events_handler from "./handlers/events_handler.js";
-
 dotenv.config();
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const DISCORD_ID = process.env.DISCORD_ID;
@@ -12,13 +10,24 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 commands_handler(client, DISCORD_TOKEN, DISCORD_ID);
 events_handler(client);
 
-
-
-client.on(Events.InteractionCreate, async (interaction: any) => {
+client.on(Events.InteractionCreate, async (interaction:) => {
     const parsedClient : any = client;
-    const command = parsedClient.commands.get(interaction.commandName);
-	await command.execute(interaction);	
-}); 
+    const command = parsedClient.commands.get(interaction['commandName']);
+    console.log(command);
+    
+    try {
+		await command.execute(interaction);
+	} catch (error) {
+        console.error(error);
+             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+        });
+
+client.on('ready', () => {
+    if (client.user != undefined) {
+        console.log(`Logged in as ${client.user.tag}!`);
+    }
+});
 
 
 client.login(DISCORD_TOKEN);
